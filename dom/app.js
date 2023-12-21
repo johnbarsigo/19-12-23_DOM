3feea616d890eec07037627cd9464b17f0187889
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-21 10:35:55 +0300

    Added data.json, modified app.js

--- dom/app.js
+++ dom/app.js
@@ -6,8 +6,25 @@ document.getElementById("add_btn").addEventListener("click", function(event){
     //add content
     //append
     let ordered_list = document.getElementById("todo_list")
-    let li = document.createElement("li")
-    li.textContent = todo
+    //let li = document.createElement("li")
+    //li.textContent = todo
     //console.log(li)
-    ordered_list.appendChild(li)
+    //ordered_list.appendChild(li)
+
+    //Update up to here
+
+    fetch("./data.json") //or put url in place of "quoted in brackets"
+    .then(data => data.json())
+    .then(usable_data => {
+        
+        usable_data.todos.forEach(todo => {
+            let li = document.createElement("li")
+            li.textContent = todo.name
+            ordered_list.appendChild(li)
+        });
+    })
 })
+
+//Update from screenshot though appears to be up to date
+//Unrelated note: Install postman;
+//sudo snap install postman
